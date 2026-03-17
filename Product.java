0f58626b974d2dbd3971d946dd929716890565b6
DarkFox90 <carlospozuelos3@gmail.com>
2026-03-16 20:56:37 -0600

    añadido gitignore y creada clase product

--- Product.java
+++ Product.java
@@ -1,3 +1,24 @@
 public class Product {
+    private String categoria;
+    private String producto;
+
+    public Product(String categoria, String producto) {
+        this.categoria = categoria;
+        this.producto = producto;
+    }
+
+    public String getCategoria() {
+        return this.categoria;
+    }
+
     
+    public String getProducto() {
+        return this.producto;
+    }
+
+    @Override
+    public String toString(){
+        return categoria + "|" + producto;
+    }
+
 }
