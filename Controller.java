520f75e95da6a2413a21a319ddbacd41dd1dc121
DarkFox90 <carlospozuelos3@gmail.com>
2026-03-17 21:23:10 -0600

    creado metodo para convertir archivo a hashmap e inicio de controller

--- Controller.java
+++ Controller.java
@@ -1,3 +1,86 @@
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 public class Controller {
     
+
+    public void start() {
+        Scanner sc = new Scanner(System.in);
+        Map<String, List<String>> maps = MapFactory.LoadData(1, "Inventory.txt");
+        
+        int opcion = 0;
+
+        while (opcion != 4) {
+            System.out.println("seleccione que tipo de mapa desea usar: ");
+            System.out.println("1. HashMap");
+            System.out.println("2. TreeMap");
+            System.out.println("3. LinkedHashMap");
+            System.out.println("4. Salir");
+
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("¿Que accion desea realizar?");
+                    System.out.println("1. agregar producto ");
+                    System.out.println("2. mostrar categoria de producto");
+                    System.out.println("3. mostrar datos de producto");
+                    System.out.println("4. mostrar datos de articulos de su coleccion");
+                    System.out.println("5. mostrar producto y categoria del inventario");
+                    System.out.println("6. mostrar productos y categorias existentes");
+                    System.out.println("7. salir");
+
+                    int opcion2 = sc.nextInt();
+
+                    switch (opcion2) {
+                        case 1:
+                           
+                            break;
+                        case 2:
+
+                            break;
+                        case 3:
+                            
+                            break;
+                        case 4:
+                            
+                            break;
+                        case 5:
+
+                            break;
+                        case 6:
+                            System.out.println("Categoría|Producto");
+                            for (Map.Entry<String, List<String>> entry : maps.entrySet()) {
+                                String categoria = entry.getKey();
+                                List<String> productos = entry.getValue();
+
+                                for (String producto : productos) {
+                                    System.out.println(categoria + "|" + producto);
+                                }
+                            }
+                            break;
+                        case 7:
+                            System.out.println("gracias por utilizar el programa");
+                            sc.close();
+                            break;
+                        default:
+                            throw new AssertionError();
+                    }
+
+                    break;
+                case 2:
+                   
+                    System.out.println(maps);
+                case 3:
+                    
+                    System.out.println(maps);
+                case 4:
+                    System.out.println("gracias por usar el programa");
+                    break;
+                default:
+                    throw new AssertionError();
+            }
+        }
+        sc.close();
+    }
 }
--- MapFactory.java
+++ MapFactory.java
@@ -1,11 +1,13 @@
-import java.util.Map;
+import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.TreeMap;
 import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class MapFactory {
 
-    public static Map<String, String> createMap(int type) {
+    public static Map<String, List<String>> createMap(int type) {
         switch (type) {
             case 1:
                 return new HashMap<>(); 
@@ -16,5 +18,20 @@ public class MapFactory {
             default:
                 throw new IllegalArgumentException("Opción inválida, debe ser 1, 2 o 3");
         }
+    }
+
+    public static Map<String, List<String>> LoadData(int type, String rutaArchivo) {
+        Map<String, List<String>> maps = createMap(type);
+        List<String[]> inventory = FileReaderUtil.leerInventario(rutaArchivo);
+
+        for (String[] item : inventory) {
+            String categoria = item[0];
+            String producto = item[1];
+
+            maps.putIfAbsent(categoria, new ArrayList<>());
+            maps.get(categoria).add(producto);
+        }
+
+        return maps;
     }
 }
